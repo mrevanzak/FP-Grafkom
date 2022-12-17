ea01f299e5fcf47b39feade0f44973f57174235d
Helmi Taqiyudin <helmitaqiyudin@gmail.com>
2022-12-17 09:43:31 +0700

    control and fence change

--- src/app/Border.ts
+++ src/app/Border.ts
@@ -16,12 +16,13 @@ export class Border {
 
   private loadTexture() {
     const loader = new GLTFLoader();
-    loader.load("models/fence/scene.gltf", (gltf) => {
+    loader.load("models/fence_wood/scene.gltf", (gltf) => {
       const root = gltf.scene;
 
       let z = 0;
-      for (let i = 0; i < 16; i++) {
-        z += 0.65;
+      for (let i = 0; i < 12; i++) {
+        z += 1.4;
+        // 0.65
         const border = clone(root);
         border.position.set(0, 0, z);
         border.castShadow = true;
@@ -47,23 +48,31 @@ export class Border {
   }
 
   public placeBorder() {
-    this.border1.position.set(-27.6, 0.6, 25.25);
-    this.border1.scale.set(5, 5, 5);
+    // this.border1.position.set(-27.6, 0, 25.25);
+
+    this.border1.position.set(-25, 0, 26);
+    this.border1.scale.set(3, 3, 3);
     this.border1.rotation.y = Math.PI / 2;
     this.scene.add(this.border1);
 
-    this.border2.position.set(26, 0.6, 27);
-    this.border2.scale.set(5, 5, 5);
+    // this.border2.position.set(26, 0, 27);
+    
+    this.border2.position.set(25.4, 0, 26);
+    this.border2.scale.set(3, 3, 3);
     this.border2.rotation.y = Math.PI;
     this.scene.add(this.border2);
 
-    this.border3.position.set(27.6, 0.6, -26);
-    this.border3.scale.set(5, 5, 5);
+    // // this.border3.position.set(27.6, 0, -26);
+
+    this.border3.position.set(25, 0, -26);
+    this.border3.scale.set(3, 3, 3);
     this.border3.rotation.y = -Math.PI / 2;
     this.scene.add(this.border3);
 
-    this.border4.position.set(-26, 0.6, -28);
-    this.border4.scale.set(5, 5, 5);
+    // // this.border4.position.set(-26, 0, -28);
+
+    this.border4.position.set(-25.4, 0, -26);
+    this.border4.scale.set(3, 3, 3);
     this.border4.rotation.y = 0;
     this.scene.add(this.border4);
   }
